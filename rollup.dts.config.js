fbe3931454b36f1bea0168f5c39147a9d4f9d4dd
gaddario98 <gaddario98@gmail.com>
2025-11-23 11:07:56 +0100

    Fix: update package.json, tsconfig.json, and add rollup.dts.config.js for improved build and type declaration support

---
+++ rollup.dts.config.js
@@ -0,0 +1,7 @@
+import { createTypeDeclarations } from "../../rollup.common.config.js";
+
+// Definizione degli entry points (deve essere sincronizzata con rollup.config.js)
+const entries = [{ name: "index", input: "index.ts" }];
+
+// Configurazione per le dichiarazioni TypeScript
+export default createTypeDeclarations(entries);
